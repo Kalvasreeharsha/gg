9ec74a97e1e9e5be3a2c8663c373868e20fb0b41
Kalvasreeharsha <harshareddykalva@gmail.com>
2025-07-17 13:03:41 +0530

    Update Big.java

--- Big.java
+++ Big.java
@@ -5,10 +5,13 @@ class Big{
 	          int b = Integer.parseInt(args[1]);
         	  int c = Integer.parseInt(args[2]);
 		if(a>b && a>c)
+		{
 			System.out.println(a);
-		else if(b>a && b>c)
-                        System.out.println(b);
-		else(c>b && c>b)
+		}
+		else if(b>a && b>c){
+                        System.out.println(b);}
+		else(c>b && c>b){
                         System.out.println(c);
+		}
 	}
 }
